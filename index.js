7333bd06d4430545b9ebc72186d3b3f6e4b6f1e8
unknown <monatayde1999@gmail.com>
2024-11-28 23:38:26 +0530

    commit

--- index.js
+++ index.js
@@ -7,21 +7,15 @@ import passport from "passport";
 import { Strategy } from "passport-local";
 import env from "dotenv";
 import GoogleStratergy from "passport-google-oauth2";
-import pgSession from 'connect-pg-simple'
+import pgSession from "connect-pg-simple";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 const saltRounds = 10;
 env.config();
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(passport.initialize());
-app.use(passport.session());
 
 const db = new pg.Client({
   connectionString: process.env.DATABASE_URL,
@@ -45,6 +39,11 @@ db.connect()
   .catch((err) => {
     console.error("Database connection error:", err.stack);
   });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // const db = new pg.Client({
 //   user: process.env.PG_USER,
